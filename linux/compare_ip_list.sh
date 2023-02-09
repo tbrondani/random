3180bb79bb98542d042cabf339c0b04f79aafd4b
Tales <40150118+tbrondani@users.noreply.github.com>
2023-02-09 11:15:37 -0300

    Updating and translating the script to english

--- linux/compare_ip_list.sh
+++ linux/compare_ip_list.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 
-# Nome dos arquivos que armazenam as listas de IPs
+# Files to work with
 yesterday_file="ips_yesterday.txt"
 today_file="ips_today.txt"
 
-# Função para ler a lista de IPs de um arquivo
+# Function to read files
 function read_ips {
   local filename="$1"
   cat "$filename"
 }
 
-# Lê as listas de IPs de cada arquivo
+# Actual reading of the files
 yesterday_ips=$(read_ips "$yesterday_file")
 today_ips=$(read_ips "$today_file")
 
-# Cria um array de IPs adicionados
-added_ips=($(echo "$today_ips" | grep -Fxv "$yesterday_ips"))
+echo "##########"
+echo "Baseline IPs from yesterday:"
+echo "$yesterday_ips"
+echo "##########"
 
-# Cria um array de IPs removidos
+# Creating arrays for yesterday and today
+added_ips=($(echo "$today_ips" | grep -Fxv "$yesterday_ips"))
 removed_ips=($(echo "$yesterday_ips" | grep -Fxv "$today_ips"))
 
-# Verifica se houve mudanças nas listas de IPs
+# Checking changes
 if [ ${#added_ips[@]} -gt 0 ] || [ ${#removed_ips[@]} -gt 0 ]; then
-  # Imprime as diferenças
+  # echoing stuff
   if [ ${#added_ips[@]} -gt 0 ]; then
-    echo "Added IPs:"
+    echo "Added IPs from yesterday:"
     for ip in "${added_ips[@]}"; do
       echo "$ip"
     done
   fi
 
   if [ ${#removed_ips[@]} -gt 0 ]; then
-    echo "Removed IPs:"
+    echo "Removed IPs from yesterday:"
     for ip in "${removed_ips[@]}"; do
       echo "$ip"
     done
   fi
 else
-  # Nenhuma mudança foi encontrada
-  echo "OK"
+  # Nothing changed in the land of ips
+  echo "Nothing changed"
 fi
