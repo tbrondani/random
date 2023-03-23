65557b69f491dac43a5e981d9a3d6800a3926fdf
Tales <40150118+tbrondani@users.noreply.github.com>
2023-03-23 17:00:44 -0300

    adding sed parameter
    adding the sed parameter to remove the last comma of the file in order to work properly with ctrl-c ctrl-v

--- linux/filter_aws_accounts_aws_alternate_contacts.sh
+++ linux/filter_aws_accounts_aws_alternate_contacts.sh
@@ -2,13 +2,12 @@
 ##  export the JSON to a bucket s3, using the LIST option.
 
 # For Billing Lists
-cat list.json | jq '.AlternateContact | to_entries[] | select(.value.Billing == "Null") | .key' | tr '\n' ',' | sed s/\"//g > super.txt
+cat list.json | jq '.AlternateContact | to_entries[] | select(.value.Billing == "Null") | .key' | tr '\n' ',' | sed 's/\"//g ; $s/,$//' > list.txt
 
 # For Security Lists
-cat list.json | jq '.AlternateContact | to_entries[] | select(.value.Security == "Null") | .key' | tr '\n' ',' | sed s/\"//g > super.txt
+cat list.json | jq '.AlternateContact | to_entries[] | select(.value.Security == "Null") | .key' | tr '\n' ',' | sed 's/\"//g ; $s/,$//' > list.txt
 
 #This will generate a oneliner in the following format to be applied later using the UPDATE option in the aws-organizations-alternate-contact-manager.
-
-' 000000000001,000000000002,000000000003,000000000004.'
+' 000000000001,000000000002,000000000003,000000000004'
 
 # This will be used to alter or modify the contacts.
