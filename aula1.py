a3fd8407637a6f5e545a800d4a12ebb2080c3ed2
Tales <40150118+tbrondani@users.noreply.github.com>
2023-05-27 10:32:53 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,78 @@
+
+##stuff aula 1
+
+# # this is a comment
+
+# '''this is also a comment,
+# a big multiline comment
+# '''
+
+
+# var = 1
+# print("hello world")
+
+# number = str(10)
+# numberr = 10
+# bariavel = float("10.5")
+
+# secops = ['tales', 'atila', 'rubens', 'fillipe', 'jon']
+
+# print(type(bariavel)) # float
+# print(type(number)) # str
+# print(type(numberr)) #int
+# print(type(secops)) #list
+
+##### operações com numeros 
+
+# n1 = 5
+# n2 = 2
+
+# soma = n1 + n2 
+# subtracao = n1 - n2 
+# multiplicacao = n1 * n2
+# divisao = n1 / n2 
+# div_inteira = n1 // n2
+# exponenciacao = n1 ** n2 
+# modulo = n1 % n2 
+
+
+########## utilizar inputs no python
+
+# n1 = int(input("insira o valor 1: "))
+# n2 = int(input("insira o valor 2: "))
+# print(f"valor numero 1: {n1}\nvalor numero 2: {n2}")
+# print(f"a soma dos nums é: {n1 + n2}")
+# soma = n1 + n2
+
+# print (f"a soma eh: {soma}")
+
+################
+
+# exercicios semana 1 #
+
+
+
+
+
+#exercises 
+
+
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+# Faça um Programa que peça dois números e imprima a soma.
+
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+# Faça um Programa que converta metros para centímetros.
+
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+# # Faça um Programa que pergunte quanto você ganha por mês e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
