5d7af4e7607c89dc157c34f88c16bc9a0d40b125
Tales <40150118+tbrondani@users.noreply.github.com>
2023-05-29 11:11:53 -0300

    done exercicios aula1.py

--- python/aula1.py
+++ python/aula1.py
@@ -47,33 +47,73 @@
 
 # print (f"a soma eh: {soma}")
 
-################
-
+########################################################################################################################
 # exercicios semana 1 #
-
-
-
-
-
-#exercises 
-
-
 # Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+##print("helloworld")
 
 # Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
+# n1 = int(input("insira o numero: "))
+# print(f"o numero eh: {n1}")
+
 # Faça um Programa que peça dois números e imprima a soma.
+
+#n1 = int(input("insira o valor 1: "))
+#n2 = int(input("insira o valor 2: "))
+#print(f"valor numero 1: {n1}\nvalor numero 2: {n2}")
 
 # Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
+# n1 = int(input("insira nota 1: "))
+# n2 = int(input("insira nota 2: "))
+# n3 = int(input("insira nota 3: "))
+# n4 = int(input("insira nota 4: "))
+
+# print(f"a media eh: {(n1+n2+n3+n4)/4}")
+
+
 # Faça um Programa que converta metros para centímetros.
+
+# n1 = int(input("insira qtde de metros: "))
+
+# print(f"vc disse: {n1}")
+# print(f"convertido eh: {n1 * 1000}cm")
 
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
+# n1 = int(input("insira qtde de metros: "))
+
+# print(f"vc disse: {n1}")
+# print(f"convertido eh: {n1 * 1000}cm")
+
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+# n1 = int(input("insira qtde de $$$ por hora: "))
+# n2 = int(input("insira qtde de horas: "))
+# print(f"vc disse $$$: {n1}")
+# print(f"vc disse horas: {n2}")
+# print(f"seu salario bruto eh: R${n1 * n2}")
 
 # # Faça um Programa que pergunte quanto você ganha por mês e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
 
+# n1 = int(input("insira salario: "))
+# n2 = int(input("insira qtde de horas: "))
+# print(f"vc disse $$$: {n1}")
+# print(f"vc disse horas: {n2}")
+# print(f"seu salario bruto por hora eh: R${n1 / n2}")
+
 # Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 
-# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+# n1 = int(input("graus fahrenheit: "))
+# transform = round((n1 - 32) * 5/9, 2)
+# print(f"a temp de {n1} Fº em Cº eh: {transform} ")
+
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus 
+# Fahrenheit.
+
+# n1 = int(input("graus celsius: "))
+# transform= (n1 * 1.8)+(32)
+# print(f"a temp de {n1} Cº em Fº eh: {transform} ")
+
