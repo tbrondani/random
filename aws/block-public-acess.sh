f9fbc2e966836812f6cfae3924b4887c74798dac
T4les <40150118+tbrondani@users.noreply.github.com>
2024-02-13 14:26:32 -0300

    Update block-public-acess.sh
    small upgrades and text adjustment

--- aws/block-public-acess.sh
+++ aws/block-public-acess.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-msg="Select action: "
-echo "$msg Select an action below"
-echo "If you find this script helpful, consider buying me a coffee: https://www.buymeacoffee.com/tbrondani"
+echo "Select an action below - please refer to my article for more info."
+echo "If you find this script helpful
+echo "consider buying me a coffee: https://www.buymeacoffee.com/tbrondani""
 
-actions=("Reading the current state " "Applying public blocks" "Reverting the changes or force public config" "Quit")
+PS3="Select a number from 1 to 3 or press 'q' to quit:"
 
-select action in "${actions[@]}"
+options=(" Get the current state of config " " Apply restrictions to EC2 and S3 public" " Disable the blocks and private configs" " Press "q" to Quit")
+
+select option in "${options[@]}"
 do
     case $REPLY in
         1)
@@ -23,13 +25,13 @@ do
             ;;
         2)
             echo "Applying the block"
-            read -p "Do you want to apply changes? (y/n): " confirm
+            read -p "Are you sure? (y/n): " confirm
             if [[ $confirm == [yY] ]]; then
                 aws ec2 enable-ebs-encryption-by-default ; echo "## EBS Encryption ENABLED"
                 aws ec2 enable-snapshot-block-public-access --state block-all-sharing ; echo " ## Snapshot public access BLOCKED"
                 aws ec2 enable-image-block-public-access --image-block-public-access-state block-new-sharing ; echo " ## AMI public access BLOCKED"
                 ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": true, "IgnorePublicAcls": true, "BlockPublicPolicy": true, "RestrictPublicBuckets": true}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID} ; echo "## S3 Public block ENABLED  ##"
-                echo "$action done"
+                echo "$option DONE!"
                 break
             else
                 echo "Changes not applied."
@@ -37,19 +39,19 @@ do
             ;;
         3)
             echo "Disabling blocks"
-            read -p "Do you want to disable and rollback changes? (y/n): " confirm
+            read -p "You sure that you want to disable and rollback changes? (y/n): " confirm
             if [[ $confirm == [yY] ]]; then
                 aws ec2 disable-ebs-encryption-by-default ; echo "## EBS Encryption DISABLED"
                 aws ec2 disable-snapshot-block-public-access ; echo " ## Snapshot public access UNRESTRICTED"
                 aws ec2 disable-image-block-public-access ; echo " ## AMI public access UNRESTRICTED"
                 ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": false, "IgnorePublicAcls": false, "BlockPublicPolicy": false, "RestrictPublicBuckets": false}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID} ; echo "## S3 Public block UNRESTRICTED"
-                echo "$action done"
+                echo "$option DONE!"
                 break
             else
                 echo "Changes not rolled back, everything stays the same!"
             fi
             ;;
-        q)
+        [Qq])
             echo "Bye and thanks for the fish! If you enjoyed this script, consider buying me a coffee: https://www.buymeacoffee.com/tbrondani"
             break
             ;;
@@ -58,3 +60,4 @@ do
             echo "Ooops - unknown choice $REPLY, hit 'q' if you want to leave the script ";;
     esac
 done
+
