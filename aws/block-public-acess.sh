d03966dfaa58c9006235777e291e30c95fa9753f
T4les <40150118+tbrondani@users.noreply.github.com>
2024-02-13 14:06:21 -0300

    Update block-public-acess.sh
    v2 with improvements

--- aws/block-public-acess.sh
+++ aws/block-public-acess.sh
@@ -25,11 +25,12 @@ do
             echo "Applying the block"
             read -p "Do you want to apply changes? (y/n): " confirm
             if [[ $confirm == [yY] ]]; then
-                aws ec2 enable-ebs-encryption-by-default
-                aws ec2 enable-snapshot-block-public-access-state --state block-all-sharing
-                aws ec2 enable-image-block-public-access-state --image-block-public-access-state block-new-sharing
-                ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": true, "IgnorePublicAcls": true, "BlockPublicPolicy": true, "RestrictPublicBuckets": true}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID}
+                aws ec2 enable-ebs-encryption-by-default ; echo "## EBS Encryption ENABLED"
+                aws ec2 enable-snapshot-block-public-access --state block-all-sharing ; echo " ## Snapshot public access BLOCKED"
+                aws ec2 enable-image-block-public-access --image-block-public-access-state block-new-sharing ; echo " ## AMI public access BLOCKED"
+                ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": true, "IgnorePublicAcls": true, "BlockPublicPolicy": true, "RestrictPublicBuckets": true}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID} ; echo "## S3 Public block ENABLED  ##"
                 echo "$action done"
+                break
             else
                 echo "Changes not applied."
             fi
@@ -38,11 +39,12 @@ do
             echo "Disabling blocks"
             read -p "Do you want to disable and rollback changes? (y/n): " confirm
             if [[ $confirm == [yY] ]]; then
-                aws ec2 disable-ebs-encryption-by-default
-                aws ec2 disable-snapshot-block-public-access
-                aws ec2 disable-image-block-public-access-state
-                ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": false, "IgnorePublicAcls": false, "BlockPublicPolicy": false, "RestrictPublicBuckets": false}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID}
+                aws ec2 disable-ebs-encryption-by-default ; echo "## EBS Encryption DISABLED"
+                aws ec2 disable-snapshot-block-public-access ; echo " ## Snapshot public access UNRESTRICTED"
+                aws ec2 disable-image-block-public-access ; echo " ## AMI public access UNRESTRICTED"
+                ID=$(aws sts get-caller-identity --query "Account" --output text) ; aws s3control put-public-access-block --public-access-block-configuration '{"BlockPublicAcls": false, "IgnorePublicAcls": false, "BlockPublicPolicy": false, "RestrictPublicBuckets": false}' --account-id ${ID} ; aws s3control get-public-access-block --account-id ${ID} ; echo "## S3 Public block UNRESTRICTED"
                 echo "$action done"
+                break
             else
                 echo "Changes not rolled back, everything stays the same!"
             fi
@@ -56,4 +58,3 @@ do
             echo "Ooops - unknown choice $REPLY, hit 'q' if you want to leave the script ";;
     esac
 done
-
