0b0dc96cf975b1b6bf1ab2caf0bd7fb8632b876e
T4les <40150118+tbrondani@users.noreply.github.com>
2024-05-25 12:39:39 -0300

    Create create-ec2-from-custom-ami.sh

---
+++ aws/create-ec2-from-custom-ami.sh
@@ -0,0 +1,9 @@
+#create a EC2 via aws cli - quick win
+aws ec2 run-instances \
+--image-id ami-12312312312312312 \
+--instance-type t2.micro \
+--count 1 \
+--subnet-id subnet-123456789123 \
+--key-name yourkey \
+--security-group-ids 'sg-xxxxxxxxxxxxxxx' \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2MACHINE},{Key=environment,Value=DEV},{Key=billing,Value=cloud},{Key=payer,Value=organization}]'
