91ff02902567942b814d7a5ac75a91d873aa5099
T4les <40150118+tbrondani@users.noreply.github.com>
2024-08-28 10:53:26 -0300

    Create query_rds_status.sh
    adding upgrades from old script

---
+++ aws/query_rds_status.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# get json from all dbs and make a table format of it, no need to use loop as jq do the heavy lifting for us
+
+# Output example:
+# Database Name: supermegaultradatabase
+# Instance Size: db.t3.medium
+# Multi-AZ: true/false
+# Disk Type: gp2 / gp3 / standard
+# Encrypted: true/false ( just returns the value of the field)
+# Size (GB): 20 (min 20gbs)
+# Max Storage (GB): 40 or N/A
+
+# known issues
+# Clustered DBs
+#  - 1 replicas always use standard disks and sizing is 1
+#  - max storage and multi-az bugs as well
+
+echo '### Listing all databses and it\'s statuses ###'
+aws rds describe-db-instances \
+    --query 'DBInstances[*]' \
+    --output json | \
+    jq -r '.[] | 
+        "Database Name: \(.DBInstanceIdentifier)\n\
+Instance Size: \(.DBInstanceClass)\n\
+Multi-AZ: \(.MultiAZ)\n\
+Disk Type: \(.StorageType)\n\
+Encrypted: \(.StorageEncrypted)\n\
+Size (GB): \(.AllocatedStorage)\n\
+Max Storage (GB): \(.MaxAllocatedStorage // \"N/A\")\n\
+---"'
+
+echo "### done ###"
